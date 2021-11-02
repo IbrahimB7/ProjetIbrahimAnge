ee2fec6f0c220ba80fb7143652872c83399d4c0e
IbrahimB7 <91197648+IbrahimB7@users.noreply.github.com>
2021-11-02 11:48:28 +0100

    main.py changement
    ajout de test pour toutes les classes

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-#!/bin/env python3 
+#!/bin/env python3
 
-from bs4 import BeautifulSoup
-from Projet import Ressource
-from Projet import Collecte
-from Projet import Traitement
-from Projet import Prisme
+from classe import Ressource 
+from classe import Collecte
+from classe import Traitement
+from classe import Prisme 
 
 u1 = "http://math.univ-angers.fr"
 u2 = "https://math.univ-angers.fr/documents/exercices_terminale_septembre_2014.pdf"
 
 
+""""
+une_ressource = Ressource(u)
+une_ressource.type()
+une_ressource.text()
+"""
+""""
+u = [u1,u2]
+une_collecte = Collecte(u)
+une_collecte.run()
+une_collecte.content()
+"""
+
+"""
+le_traitement = Traitement()
+le_traitement.load(une_collecte.textes)
+le_traitement.run()         # génère le résultat
+le_traitement.show()        # affiche le résultat
+"""
+
+"""
 U = [u1,u2]
 un_prisme = Prisme(Traitement)
 un_prisme.run(U)
 un_prisme.show()
+"""
