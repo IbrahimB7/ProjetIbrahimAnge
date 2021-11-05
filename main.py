9764a6970a048ea3310a66f9dccfc4192433fa4d
IbrahimB7 <91197648+IbrahimB7@users.noreply.github.com>
2021-11-05 13:52:00 +0100

    Update main.py
    petite modifications

--- main.py
+++ main.py
@@ -5,29 +5,31 @@ from Projet import *
 u1 = "http://math.univ-angers.fr"
 u2 = "https://math.univ-angers.fr/documents/exercices_terminale_septembre_2014.pdf"
 
-
-""""
+"""
+u = u2
 une_ressource = Ressource(u)
 une_ressource.type()
 une_ressource.text()
+print(une_ressource.texte)
 """
-""""
+"""
+
 u = [u1,u2]
 une_collecte = Collecte(u)
 une_collecte.run()
 une_collecte.content()
-"""
 
-"""
+
+
+
 le_traitement = Traitement()
 le_traitement.load(une_collecte.textes)
 le_traitement.run()         # génère le résultat
 le_traitement.show()        # affiche le résultat
 """
-
 """
 U = [u1,u2]
 un_prisme = Prisme(Traitement)
 un_prisme.run(U)
-un_prisme.show()
-"""
+un_prisme.show()"""
+
