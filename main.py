af17b1778ae2344a50e8a96fc8df308c4a060ad8
IbrahimB7 <91197648+IbrahimB7@users.noreply.github.com>
2021-11-28 15:27:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,10 @@
 
 from Projet import *
 
-u1 = "http://math.univ-angers.fr"
-u2 = "https://math.univ-angers.fr/documents/exercices_terminale_septembre_2014.pdf"
 
-"""
-u = u2
-une_ressource = Ressource(u)
-une_ressource.type()
-une_ressource.text()
-print(une_ressource.texte)
-"""
+from Projet import Prisme
 
-"""
-u = [u1,u2]
-une_collecte = Collecte(u)
-une_collecte.run()
-print(une_collecte.content())
-"""
-
-
-"""
-le_traitement = Traitement()
-le_traitement.load(une_collecte.textes)
-le_traitement.run()         # génère le résultat
-le_traitement.show()        # affiche le résultat
-"""
-"""
-U = [u1,u2]
-un_prisme = Prisme(Traitement)
-un_prisme.run(U)
-un_prisme.show()"""
+un_prisme = Prisme('Nuage')
+un_prisme.run('text.txt')
+un_prisme.show()
 
