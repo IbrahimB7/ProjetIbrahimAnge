a0fc97e32cac261719247d053a70e6a1a9aecbb7
IbrahimB7 <91197648+IbrahimB7@users.noreply.github.com>
2021-11-28 15:28:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 #!/bin/env python3
-
-from Projet import *
-
-
-from Projet import Prisme
-
+from Projet2 import Prisme
+"""Avant de commencer, il faut créer un fichier texte 'text.txt' dans le dossier avec tous les liens"""
 un_prisme = Prisme('Nuage')
 un_prisme.run('text.txt')
 un_prisme.show()
