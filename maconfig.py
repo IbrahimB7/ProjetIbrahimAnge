4c5fb76ae6bf4087614bd751783339398ebf610e
IbrahimB7 <91197648+IbrahimB7@users.noreply.github.com>
2021-12-06 15:54:28 +0100

    Add files via upload

---
+++ maconfig.py
@@ -0,0 +1,16 @@
+#!/bin/env python3 
+
+'''Lire le fichier requirements.txt pour voir les modules à installer!'''
+
+
+urls = ['https://math.univ-angers.fr/','https://math.univ-angers.fr/documents/exercices_terminale_septembre_2014.pdf']  #Liste de liens 
+
+navigateurpath = None  #Navigateur par défaut choisi, remplacer None par 'PATH' avec PATH le chemin du navigateur à choisir (Ne semble pas fonctionner sur windows?)
+
+
+#Paramètres nuage de mots
+mots_exclus = ['de']  #Ecrire les mots à exclure entre ''  
+nombre_mots = 100 #Choisir le nombre de mot à avoir dans le nuage de mot
+couleur_fond = 'white' #Choisir la couleur du fond 
+
+
